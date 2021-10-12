899e1982003227591323b3a13e38d3bc4fd87503
PROMETEC-ITSSMT <juarezmartinez.juliocesar@gmail.com>
2021-10-12 14:16:09 -0500

    Carga inicial
    Carga inicial

---
+++ Laura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
