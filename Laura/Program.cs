2149d4ff2e595d21363667a1db6f7aaf8771f724
Julio César Juárez Martínez <julio.juarez@nexta.biz>
2021-10-12 14:19:22 -0500

    Probando rama main

--- Laura/Program.cs
+++ Laura/Program.cs
@@ -7,6 +7,7 @@ namespace Laura
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Probando GIT");
         }
     }
 }
