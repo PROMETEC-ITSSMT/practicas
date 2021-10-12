d96b294d78520a005e2f871555ee16dc961ce8c9
Julio César Juárez Martínez <julio.juarez@nexta.biz>
2021-10-12 14:57:30 -0500

    Cambios de constructores

---
+++ Laura/Operaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laura
+{
+    class Operaciones
+    {
+        static void Main(string[] args)
+        {
+            Suma suma1 = new Suma();
+
+
+            Suma suma2 = new Suma(2);
+
+
+            int resulatdoSuma1 = suma2.suma_2num(2, 3);
+
+            Console.WriteLine(resulatdoSuma1);
+        }
+    }
+}
---
+++ Laura/Suma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Laura
+{
+    class Suma
+    {
+        public int a; 
+        // Constructor con un parametro
+        public Suma()
+        {
+            Console.WriteLine("Constructor sin parametros");
+        }
+        // Constructor con dos parametros
+        public Suma(int a) {
+            this.a = a;
+            Console.WriteLine("Constructor con un parametro: {0} " , a);
+        }
+        // Metodo de suma
+        public int suma_2num(int num1, int num2) {
+            int resultado = num1 + num2 + this.a;
+
+            return resultado; 
+        }
+    }
+}
